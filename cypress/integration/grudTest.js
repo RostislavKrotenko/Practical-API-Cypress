0d3e65e59ce8402b38b303152b62aa1fac0c95fa
RostislavKrotenko <rostik.krotenko@gmail.com>
2023-11-22 16:50:13 +0200

    test: POST and GET

--- cypress/integration/grudTest.js
+++ cypress/integration/grudTest.js
@@ -7,6 +7,7 @@ describe('Testing GRUD operations', () => {
     })
 
     it('Test POST and DELETE operations', () => {
+
         const bodyRequest = {
             "article": {
                 "title": "Request from API",
@@ -25,7 +26,6 @@ describe('Testing GRUD operations', () => {
                 body: bodyRequest
             }).then( response => {
                 const slug = response.body.article.slug
-                console.log(Cypress.env("apiURL")+'/api/articles/'+ slug)
                 cy.request({ 
                     url: Cypress.env("apiURL")+'/api/articles/'+ slug,
                     headers: {'Authorization': 'Token ' + token},
@@ -33,5 +33,46 @@ describe('Testing GRUD operations', () => {
                 })
             })
         })
+    })
+
+    it('Test POST and GET request', () => {
+        
+        
+        const bodyRequest = {
+            "article": {
+                "title": "POST and GET",
+                "description": "POST and GET test operations",
+                "body": "tryng to use POST and GET operations during testing",
+                "tagList": []
+            }
+        }
+
+        cy.get('@token').then( token => {
+
+            cy.request({
+                url: Cypress.env("apiURL")+'/api/articles/',
+                headers: {'Authorization': 'Token ' + token},
+                method: 'POST',
+                body: bodyRequest
+            }).then( response => {
+                const slug = response.body.article.slug
+
+                cy.request({ 
+                    url: Cypress.env("apiURL")+'/api/articles/',
+                    headers: {'Authorization': 'Token ' + token},
+                    method: 'GET',
+                }).its('body').then(body => {
+                    expect(body.articles[0].title).to.equal('POST and GET')
+                    expect(body.articles[0].description).to.equal("POST and GET test operations")
+                })
+
+                cy.request({ 
+                    url: Cypress.env("apiURL")+'/api/articles/'+ slug,
+                    headers: {'Authorization': 'Token ' + token},
+                    method: 'DELETE'
+                })
+            })
+            
+        })   
     })
 })
